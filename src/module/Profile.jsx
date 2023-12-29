e90662cd51b31b50a06c64fefca83472260ecd64
jingoochoi <jingoo0206@gmail.com>
2023-12-29 18:28:44 +0900

    Update Profile.jsx

--- src/module/Profile.jsx
+++ src/module/Profile.jsx
@@ -2,18 +2,21 @@ import { useState } from 'react'
 import '../css/profile.css'
 export function Profile() {
     const[name,setName]=useState(1)
+    const mola=(a)=>{
+        setName(a)
+    }
     return(
         <>
             <h1>PROFILE</h1>
             {
                 name===1&&<div className='bx'>
                 <h2>이름: 최진구</h2>
-                <div className="baka">🔁</div></div>
+                <div className="baka" onClick={()=>mola(0)}>🔁</div></div>
             }
             {
                 name===0&&<div className='bx'>
                 <h2>name: tony jingoo choi</h2>
-                <div className="baka">🔁</div></div>
+                <div className="baka" onClick={()=>mola(1)}>🔁</div></div>
             }
         </>
     )
