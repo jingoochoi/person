3e1ba1359ab8e4ad4a358929f283f2e595cce434
jingoochoi <jingoo0206@gmail.com>
2024-01-02 14:17:56 +0900

    Update Map.jsx

--- src/module/Map.jsx
+++ src/module/Map.jsx
@@ -5,11 +5,11 @@ import { Loader } from "@googlemaps/js-api-loader"
 
 export function Map() {
   const loader = new Loader({
-    apiKey: "YOUR_API_KEY",
+    apiKey: "AIzaSyBVCB_65HYOI5VTkuNwW08SMv9JOfx7Rdo",
     version: "weekly",
     // ...additionalOptions,
   });
-  let map
+  let map,infoWindow
   loader.load().then(async () => {
     const { Map } = await window.google.maps.importLibrary("maps");
   
@@ -17,7 +17,51 @@ export function Map() {
       center: { lat: -34.397, lng: 150.644 },
       zoom: 8,
     });
+    infoWindow=new window.google.maps.InfoWindow()
   });
+  function initMap() {
+    const locationButton = document.createElement("button");
+
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[window.google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+  }
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+      browserHasGeolocation
+        ? "Error: The Geolocation service failed."
+        : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(map);
+  }
+  
+  window.initMap = initMap;
+  
     return(
         <>
             <h1>MAP</h1>
