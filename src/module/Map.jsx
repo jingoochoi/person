d746f7126be61bcce38bf51dbd2afa526ca5d1b0
jingoochoi <jingoo0206@gmail.com>
2024-01-03 09:57:34 +0900

    Update Map.jsx

--- src/module/Map.jsx
+++ src/module/Map.jsx
@@ -2,37 +2,11 @@ import { GoogleMap, useGoogleMap } from '@react-google-maps/api';
 import '../css/map.css'
 // import { GoogleMap } from '@react-google-maps/api';
 import { Loader } from "@googlemaps/js-api-loader"
+import { useCallback, useEffect, useRef } from 'react';
 
 export function Map() {
-  const loader = new Loader({
-    apiKey: "AIzaSyBVCB_65HYOI5VTkuNwW08SMv9JOfx7Rdo",
-    version: "weekly",
-    // ...additionalOptions,
-  });
-  // Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
-let map, infoWindow;
-
-function initMap() {
-  loader.load().then(async () => {
-    const { Map } = await window.google.maps.importLibrary("maps");
-  
-    map = new Map(document.getElementById("map"), {
-      center: { lat: -34.397, lng: 150.644 },
-      zoom: 8,
-    });
-  }).catch(e=>console.log(e));
-  infoWindow = new window.google.maps.InfoWindow();
-
-  const locationButton = document.createElement("button");
-
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[window.google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
+  let map,infoWindow
+  const initMap = useCallback(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
@@ -41,33 +15,37 @@ function initMap() {
             lng: position.coords.longitude,
           };
 
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          infoWindow.open(map);
+          // infoWindow.setPosition(pos);
+          // infoWindow.setContent("Location found.");
+          // infoWindow.open(map);
           map.setCenter(pos);
         },
         () => {
           handleLocationError(true, infoWindow, map.getCenter());
-        },
+        },{enableHighAccuracy:true}
       );
     } else {
       // Browser doesn't support Geolocation
       handleLocationError(false, infoWindow, map.getCenter());
     }
-  });
-}
+    map=new window.google.maps.Map(document.querySelector('#map'), {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 13,
+    });
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(
+        browserHasGeolocation
+          ? "Error: The Geolocation service failed."
+          : "Error: Your browser doesn't support geolocation."
+      );
+      infoWindow.open(map);
+    }
+  }, []);
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation.",
-  );
-  infoWindow.open(map);
-}
-
-window.initMap = initMap;
+  useEffect(() => {
+    initMap();
+  }, [initMap]);
     return(
         <>
             <h1>MAP</h1>
