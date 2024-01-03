42c2f8c7f9ee36b368423dbec582fe944f0d215a
jingoochoi <jingoo0206@gmail.com>
2024-01-03 17:37:24 +0900

    Update Link.jsx

--- src/module/Link.jsx
+++ src/module/Link.jsx
@@ -1,5 +1,10 @@
 export function Link() {
     return(
-        <></>
+        <>
+            <h1>LINK</h1>
+            <a href="#" onClick={window.open('https://jingoochoi.github.io/football/','',`width=300,height=${300*2},top=${window.innerHeight/2-300},left=${window.innerWidth/2-300/2}`)}>FOOTBALL SOCCER GAME</a>
+            <a href="#" onClick={window.open('https://jingoochoi.github.io/gobase/','',`width=300,height=${300*2},top=${window.innerHeight/2-300},left=${window.innerWidth/2-300/2}`)}>GOBASE BASEBALL GAME</a>
+            <a href="#" onClick={window.open('https://jingoochoi.github.io/tonypj2/','_blank')}>GAME INFORMATION WEB</a>
+        </>
     )
 }
