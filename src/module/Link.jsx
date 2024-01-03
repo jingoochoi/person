60459a54cce70e5aa0648d340b3c07a1c63c515e
jingoochoi <jingoo0206@gmail.com>
2024-01-03 17:58:25 +0900

    Update Link.jsx

--- src/module/Link.jsx
+++ src/module/Link.jsx
@@ -1,10 +1,13 @@
 export function Link() {
+    const foot=()=>{window.open('https://jingoochoi.github.io/football/','',`width=300,height=${300*2},top=${window.innerHeight/2-300},left=${window.innerWidth/2-300/2}`)}
+    const base=()=>{window.open('https://jingoochoi.github.io/gobase/','',`width=300,height=${300*2},top=${window.innerHeight/2-300},left=${window.innerWidth/2-300/2}`)}
+    const game=()=>{window.open('https://jingoochoi.github.io/tonypj2/','_blank')}
     return(
         <>
             <h1>LINK</h1>
-            <a href="#" onClick={window.open('https://jingoochoi.github.io/football/','',`width=300,height=${300*2},top=${window.innerHeight/2-300},left=${window.innerWidth/2-300/2}`)}>FOOTBALL SOCCER GAME</a>
-            <a href="#" onClick={window.open('https://jingoochoi.github.io/gobase/','',`width=300,height=${300*2},top=${window.innerHeight/2-300},left=${window.innerWidth/2-300/2}`)}>GOBASE BASEBALL GAME</a>
-            <a href="#" onClick={window.open('https://jingoochoi.github.io/tonypj2/','_blank')}>GAME INFORMATION WEB</a>
+            <a href="#" onClick={foot}>FOOTBALL SOCCER GAME</a>
+            <a href="#" onClick={base}>GOBASE BASEBALL GAME</a>
+            <a href="#" onClick={game}>GAME INFORMATION WEB</a>
         </>
     )
 }
