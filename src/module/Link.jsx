4834b16c69843fcdd15a39f3614bedf2449cf112
jingoochoi <jingoo0206@gmail.com>
2024-01-03 18:20:40 +0900

    .

--- src/module/Link.jsx
+++ src/module/Link.jsx
@@ -1,3 +1,4 @@
+import '../css/link.css'
 export function Link() {
     const foot=()=>{window.open('https://jingoochoi.github.io/football/','',`width=300,height=${300*2},top=${window.innerHeight/2-300},left=${window.innerWidth/2-300/2}`)}
     const base=()=>{window.open('https://jingoochoi.github.io/gobase/','',`width=300,height=${300*2},top=${window.innerHeight/2-300},left=${window.innerWidth/2-300/2}`)}
@@ -5,9 +6,11 @@ export function Link() {
     return(
         <>
             <h1>LINK</h1>
-            <a href="#" onClick={foot}>FOOTBALL SOCCER GAME</a>
-            <a href="#" onClick={base}>GOBASE BASEBALL GAME</a>
-            <a href="#" onClick={game}>GAME INFORMATION WEB</a>
+            <div className="link">
+                <a href="#" onClick={foot}>FOOTBALL SOCCER GAME</a>
+                <a href="#" onClick={base}>GOBASE BASEBALL GAME</a>
+                <a href="#" onClick={game}>GAME INFORMATION WEB</a>
+            </div>
         </>
     )
 }
