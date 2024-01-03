59f5f7389c74f5b6bd18acafa6890d6b923527a0
jingoochoi <jingoo0206@gmail.com>
2024-01-03 15:09:38 +0900

    Update Map.jsx

--- src/module/Map.jsx
+++ src/module/Map.jsx
@@ -34,12 +34,12 @@ export function Map() {
     });
     function handleLocationError(browserHasGeolocation, infoWindow, pos) {
       // infoWindow.setPosition(pos);
-      infoWindow.setContent(
-        browserHasGeolocation
-          ? "Error: The Geolocation service failed."
-          : "Error: Your browser doesn't support geolocation."
-      );
-      infoWindow.open(map);
+      // infoWindow.setContent(
+      //   browserHasGeolocation
+      //     ? "Error: The Geolocation service failed."
+      //     : "Error: Your browser doesn't support geolocation."
+      // );
+      // infoWindow.open(map);
     }
   }, []);
 
