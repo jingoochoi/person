66b99357c6339dcac43a90326980bba21b7905a5
jingoochoi <jingoo0206@gmail.com>
2024-01-04 09:11:56 +0900

    .

--- src/module/Map.jsx
+++ src/module/Map.jsx
@@ -23,7 +23,7 @@ export function Map() {
         },
         () => {
           handleLocationError(true, infoWindow, map.getCenter());
-        }
+        },{enableHighAccuracy:true}
       );
     } else {
       // Browser doesn't support Geolocation
