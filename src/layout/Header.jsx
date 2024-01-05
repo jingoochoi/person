930958702125746c0ee1d980ae035337bc4b0d5d
jingoochoi <jingoo0206@gmail.com>
2024-01-05 14:29:35 +0900

    Update Header.jsx

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,6 +1,8 @@
 import { useContext, useState } from 'react'
 import '../css/header.css'
 import { ctxt } from '../module/context'
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google'
+import { jwtDecode } from 'jwt-decode'
 export function Header() {
     const tt=useContext(ctxt)
     const[logg,setLogg]=useState(false)
@@ -12,8 +14,18 @@ export function Header() {
             <h3 onClick={()=>tt.chan('map')}>MAP</h3>
             <h3 onClick={()=>tt.chan('geo')}>GEOCODE</h3>
             {
-                !logg&&
+                !logg&&<>
             <h3>LOGIN</h3>
+            <GoogleOAuthProvider clientId='471246988772-6ssmcn8qdcasdo1v8kcon3iqg167a3po.apps.googleusercontent.com'>
+                <GoogleLogin onSuccess={(cr)=>{
+                    console.log(cr)
+                    let dato=jwtDecode(cr.credential)
+                    console.log(dato)
+                }} onError={()=>{
+                    console.log('error')
+                }}></GoogleLogin>
+            </GoogleOAuthProvider>
+            </>
             }
         </div>
     )
