3e4f2918f505f0ba1484880cf05fb6790e7f860b
jingoochoi <jingoo0206@gmail.com>
2024-01-09 11:21:51 +0900

    Update Header.jsx

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -35,7 +35,7 @@ export function Header() {
             }
             {
                 logg&&
-                <h3 onclick={signOut}>LOGOUT</h3>
+                <h3 onClick={signOut}>LOGOUT</h3>
             }
             <GoogleOAuthProvider clientId='471246988772-6ssmcn8qdcasdo1v8kcon3iqg167a3po.apps.googleusercontent.com'>
                 <GoogleLogin onSuccess={(cr)=>{
