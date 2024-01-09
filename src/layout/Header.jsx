a6b2057ba30bf0c30aae5417fd7efc4e233ff4a2
jingoochoi <jingoo0206@gmail.com>
2024-01-09 11:05:33 +0900

    Update Header.jsx

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -16,7 +16,7 @@ export function Header() {
         setLogg(true)
     }
     function signOut() {
-        var auth2 = gapi.auth2.getAuthInstance();
+        var auth2 = window.gapi.auth2.getAuthInstance();
         auth2.signOut().then(function () {
           console.log('User signed out.');
         });
