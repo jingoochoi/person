1c97d213cf7d67c5617b03d5fde06df6274a563e
jingoochoi <jingoo0206@gmail.com>
2024-01-10 10:02:58 +0900

    .

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -23,31 +23,34 @@ export function Header() {
           });
       }
     return(
-        <div className='head'>
-            <h3 onClick={()=>tt.chan('pro')}>PROFILE</h3>
-            <h3 onClick={()=>tt.chan('link')}>LINK</h3>
-            <h3 onClick={()=>tt.chan('map')}>MAP</h3>
-            <h3 onClick={()=>tt.chan('geo')}>GEOCODE</h3>
-            {/* {
-                !logg&&<>
-            <h3 className="g-signin2" data-onsuccess={onSignIn}>LOGIN</h3>
-            </>
-            }
-            {
-                logg&&
-                <h3 onClick={signOut}>LOGOUT</h3>
-            } */}
-            <GoogleOAuthProvider clientId='471246988772-6ssmcn8qdcasdo1v8kcon3iqg167a3po.apps.googleusercontent.com'>
-                <GoogleLogin onSuccess={(cr)=>{
-                    // console.log(cr)
-                    let dato=jwtDecode(cr.credential)
-                    // console.log(dato)
-                    // console.log(dato.alg,dato.kid,dato.typ)
-                    setLogg(true)
-                }} onError={()=>{
-                    console.log('error')
-                }}></GoogleLogin>
-            </GoogleOAuthProvider>
-        </div>
+        <>
+            <div className="mobi">≡</div>
+            <div className='head'>
+                <h3 onClick={()=>tt.chan('pro')}>PROFILE</h3>
+                <h3 onClick={()=>tt.chan('link')}>LINK</h3>
+                <h3 onClick={()=>tt.chan('map')}>MAP</h3>
+                <h3 onClick={()=>tt.chan('geo')}>GEOCODE</h3>
+                {/* {
+                    !logg&&<>
+                < className="g-signin2" data-onsuccess={onSignIn}>LOGIN</ h3>
+                </>
+                }
+                {
+                    logg&&
+                    <h3 onClick={signOut}>LOGOUT</h3>
+                } */}
+                <GoogleOAuthProvider    clientId='471246988772-6ssmcn8qdcasdo1v8kcon3iqg167a3po.   apps.googleusercontent.com'>
+                    <GoogleLogin onSuccess={(cr)=>{
+                        // console.log(cr)
+                        let dato=jwtDecode(cr.credential)
+                        // console.log(dato)
+                        // console.log(dato.alg,dato.kid,dato.typ)
+                        setLogg(true)
+                    }} onError={()=>{
+                        console.log('error')
+                    }}></GoogleLogin>
+                </GoogleOAuthProvider>
+            </div>
+        </>
     )
 }
