2bdaa2593b46b58a220d3a1922f7f8f1c8ac8888
jingoochoi <jingoo0206@gmail.com>
2024-01-11 15:10:18 +0900

    Update Header.jsx

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -7,7 +7,12 @@ import $ from 'jquery'
 export function Header() {
     const tt=useContext(ctxt)
     const[logg,setLogg]=useState(false)
-    function onSignIn(googleUser) {
+    function cess(googleUser) {
+        // console.log(cr)
+        // let dato=jwtDecode(cr.credential)
+        // console.log(dato)
+        // console.log(dato.alg,dato.kid,dato.typ)
+        // setLogg(true)
         var profile = googleUser.getBasicProfile();
         console.log(profile)
         // profile.getId()
@@ -48,17 +53,13 @@ export function Header() {
                     logg&&
                     <h3 onClick={signOut}>LOGOUT</h3>
                 } */}
-                <GoogleOAuthProvider    clientId='471246988772-6ssmcn8qdcasdo1v8kcon3iqg167a3po.   apps.googleusercontent.com'>
-                    <GoogleLogin onSuccess={(cr)=>{
-                        // console.log(cr)
-                        let dato=jwtDecode(cr.credential)
-                        // console.log(dato)
-                        // console.log(dato.alg,dato.kid,dato.typ)
-                        setLogg(true)
-                    }} onError={()=>{
-                        console.log('error')
-                    }}></GoogleLogin>
-                </GoogleOAuthProvider>
+                    <GoogleLogin 
+                        clientId='471246988772-6ssmcn8qdcasdo1v8kcon3iqg167a3po.apps.googleusercontent.com'
+                        onSuccess={cess}
+                        onError={(e)=>{console.log(e)}}
+                        cookiePolicy={'single_host_origin'}
+                    >
+                    </GoogleLogin>
             </div>
         </>
     )
