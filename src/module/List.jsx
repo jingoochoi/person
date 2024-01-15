a4c5a174b99eb1da9599351ccd59df2ff8ae7157
jingoochoi <jingoo0206@gmail.com>
2024-01-15 14:49:56 +0900

    Create List.jsx

---
+++ src/module/List.jsx
@@ -0,0 +1,12 @@
+export function List() {
+    return(
+        <>
+            <div className="last">
+                <li style={{display:'flex'}}>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVVzSuPAU2pa7p72hcV4oHuK4eqH_UcDwYFw&usqp=CAU" alt="" style={{width:'300px'}}/>
+                    <p>1인용 오피스<br />서울시 강남구 역삼동<br />6,000원<br />총 120,000원</p>
+                </li>
+            </div>
+        </>
+    )
+}
