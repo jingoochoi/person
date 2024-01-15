9458c0eecc81a47aa82f14cc53a7ec604f8ffc13
jingoochoi <jingoo0206@gmail.com>
2024-01-15 14:51:04 +0900

    Update List.jsx

--- src/module/List.jsx
+++ src/module/List.jsx
@@ -2,9 +2,13 @@ export function List() {
     return(
         <>
             <div className="last">
-                <li style={{display:'flex'}}>
+                <li style={{display:'flex',marginTop:'10px'}}>
                     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVVzSuPAU2pa7p72hcV4oHuK4eqH_UcDwYFw&usqp=CAU" alt="" style={{width:'300px'}}/>
                     <p>1인용 오피스<br />서울시 강남구 역삼동<br />6,000원<br />총 120,000원</p>
+                </li>
+                <li style={{display:'flex',marginTop:'10px'}}>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVVzSuPAU2pa7p72hcV4oHuK4eqH_UcDwYFw&usqp=CAU" alt="" style={{width:'300px'}}/>
+                    <p>1인용 오피스<br />서울시 서초구 양재동<br />5,000원<br />총 100,000원</p>
                 </li>
             </div>
         </>
